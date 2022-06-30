30fcb210fdb53d902fac4e33a1d4738dcda576f4
Tevin Aduma <tevin74@live.com>
2022-07-01 02:56:17 +0300

    script that generates the assembly code (Intel syntax) of a C code and save it in an output file

---
+++ 0x00-hello_world/7-main.s
@@ -0,0 +1,68 @@
+	.file	"7-main.c"
+	.intel_syntax noprefix
+	.text
+	.section	.rodata
+.LC0:
+	.string	"Enter your name below:"
+.LC1:
+	.string	"%s"
+.LC2:
+	.string	"\n %s"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register 6
+	sub	rsp, 64
+	mov	rax, QWORD PTR fs:40
+	mov	QWORD PTR -8[rbp], rax
+	xor	eax, eax
+	lea	rdi, .LC0[rip]
+	call	puts@PLT
+	lea	rax, -64[rbp]
+	mov	rsi, rax
+	lea	rdi, .LC1[rip]
+	mov	eax, 0
+	call	__isoc99_scanf@PLT
+	lea	rax, -64[rbp]
+	mov	rsi, rax
+	lea	rdi, .LC2[rip]
+	mov	eax, 0
+	call	printf@PLT
+	mov	eax, 0
+	mov	rdx, QWORD PTR -8[rbp]
+	xor	rdx, QWORD PTR fs:40
+	je	.L3
+	call	__stack_chk_fail@PLT
+.L3:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
