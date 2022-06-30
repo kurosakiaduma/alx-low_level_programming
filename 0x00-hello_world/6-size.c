9e7735b84378bad532e545c1dbfdbc8aaab99615
Tevin Aduma <tevin74@live.com>
2022-07-01 02:44:11 +0300

    complying to Betty style

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,7 @@
  * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
-{	
+{
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
