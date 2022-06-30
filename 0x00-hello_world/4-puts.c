75612e8a0ed4846b389a3e1d3ef725b2ea38b968
Tevin Aduma <tevin74@live.com>
2022-07-01 02:15:54 +0300

    add newline to string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(){
-	puts("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle\n");
 	
 	/**
 	Exit the program after it prints the string successfully
