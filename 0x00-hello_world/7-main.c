d46ea724c0a6f2d01fd1ff7c303274de844b3d09
Tevin Aduma <tevin74@live.com>
2022-07-01 02:52:41 +0300

    C program that prints out user input

---
+++ 0x00-hello_world/7-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Simple C program that reads user input and prints
+ * out to the screen
+ */
+int main(void){
+	char f_name; 
+	puts("Enter your name below:");
+	scanf("%d", &f_name);
+	printf("\n %s", f_name);
+	return (0);
+}
