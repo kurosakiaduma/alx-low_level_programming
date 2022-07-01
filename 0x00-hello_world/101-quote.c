f83aaff23d601d3d42264525706d510a77b10ab2
Tevin Aduma <tevin74@live.com>
2022-07-01 03:32:58 +0300

    used write to print o stderr in order to comply to Betty linter

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,6 @@
  */
 int main(void)
 {
-	fprintf(stderr, "%s\n", "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (1);
 }
