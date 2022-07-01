a62e0292eceea3cb976d5c450e78fb1329004c72
Tevin Aduma <tevin74@live.com>
2022-07-01 03:24:04 +0300

    complying to Betty style

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	fprintf(stderr, "%s\n", "and that piece of\ 
-			art is useful\" - Dora Korpar, 2015-10-19");
+	fprintf(stderr, "%s\n", "and that piece of\
+art is useful\" - Dora Korpar, 2015-10-19");
 	return (1);
 }
