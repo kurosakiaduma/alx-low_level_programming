7717bad84b8d54d7de52bcf934a033b261f92e1e
Tevin Aduma <tevin74@live.com>
2022-07-01 03:34:20 +0300

    used write to print o stderr in order to comply to Betty linter

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,13 @@
 #include <errno.h>
 
 /**
- * main prints exactly and that piece of
+ * main - prints exactly and that piece of
  * art is useful\" - Dora Korpar, 2015-10-19
  * followed by a new line, to the standard error
  * Return: 1 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
 	return (1);
 }
