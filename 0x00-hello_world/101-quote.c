870f287198191c03aa94dd9cb5adf3abc08dff1b
Tevin Aduma <tevin74@live.com>
2022-07-01 03:08:30 +0300

    C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <errno.h>
+/**
+ * C program that prints exactly and that piece of
+ * art is useful\" - Dora Korpar, 2015-10-19
+ * followed by a new line, to the standard error
+ */
+int main(void)
+{
+	fprintf( stderr, "%s \n", "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
