54a0bef921ab6912d7fb656b5c6a29d3ca1a76cc
Tevin Aduma <tevin74@live.com>
2022-07-01 03:21:56 +0300

    opted to use a string pointer to store the line

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,9 +7,7 @@
  */
 int main(void)
 {
-	char *mystr = "and that piece of art "
-		"is useful\" - "
-		"Dora Korpar, 2015-10-19";
-	fprintf(stderr, "%s\n", mystr);
-	return (0);
+	fprintf(stderr, "%s\n", "and that piece of\ 
+			art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
 }
