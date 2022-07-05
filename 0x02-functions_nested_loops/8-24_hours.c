4c49b355d437f748997a08444b43a161b8599a6d
Tevin Aduma <tevin74@live.com>
2022-07-05 22:52:20 +0300

    removed trailing whitespace ~ Betty compliance

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * jack_bauer - Print every minute of the day of Agent Jack Bauer
- * Description: Start from 00:00 to 23:59 and 
+ * Description: Start from 00:00 to 23:59
  */
 void jack_bauer(void)
 {
