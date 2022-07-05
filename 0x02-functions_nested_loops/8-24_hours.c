dd89feb99ef7f5b1b170ed1fdbe6c1fb316d6ed9
Tevin Aduma <tevin74@live.com>
2022-07-05 21:01:29 +0300

    function to print hours defined

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * Jack_Bauer - Print every minute of the day of Agent Jack Bauer
+ * Description: Start from 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+	int i = 0;
+	int h1 = 0, h2 = 0, m1 = 0, m2 = 0;
+
+	while (i < 1440)
+	{
+		_putchar(h1 + '0');
+		_putchar(h2 + '0');
+		_putchar(':');
+		_putchar(m1 + '0');
+		_putchar(m2 + '0');
+		_putchar('\n');
+
+		d++;
+		if (m4 > 9)
+		{
+			m4 = 0;
+			m3++;
+		}
+		if (m3 > 5)
+		{
+			m3 = 0;
+			h2++;
+		}
+		if (h2 > 9)
+		{
+			h2 = 0;
+			h1++;
+		}
+		i++;
+	}
+}
