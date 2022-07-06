170026a0fbad09ef55be6d38c6661c9894e07e71
Tevin Aduma <tevin74@live.com>
2022-07-06 23:07:53 +0300

    removed extra code from main

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -14,13 +14,3 @@ int main(void)
 
 	return (0);
 }
-
-void positive_or_negative(int i)
-{
-	if (i > 0)
-		printf("%i is positive\n", i);
-	else if (i < 0)
-		printf("%i is negative\n", i);
-	else
-		printf("%i is zero\n", i);
-}
