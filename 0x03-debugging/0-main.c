d0f2d9603ecbf7fe7de7d0e3b0888213b00ef526
Tevin Aduma <tevin74@live.com>
2022-07-06 23:05:31 +0300

    Tested positive_negative_zero
    Successfully used user-defined function to test code

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,10 +7,20 @@
 
 int main(void)
 {
-        int i;
+	int i;
 
-        i = 98;
-        positive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
 
-        return (0);
+	return (0);
+}
+
+void positive_or_negative(int i)
+{
+	if (i > 0)
+		printf("%i is positive\n", i);
+	else if (i < 0)
+		printf("%i is negative\n", i);
+	else
+		printf("%i is zero\n", i);
 }
